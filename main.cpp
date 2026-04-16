b64f5102b108debff9e9fa3706178d6ca61e0109
Rollin Gilman <rdgilman@my.canyons.edu>
2026-04-15 20:13:32 -0700

    Q5Q2

--- main.cpp
+++ main.cpp
@@ -11,7 +11,20 @@ int main()
     cin >> M;
 
     cout << N << " to power number: ";
+    int power = 1;
 
+    if (0 < N < 10;  1 <= M <= 10)
+    {
+        for (int i=0; i<N; i++)
+        {
+        power *= M;
+        cout << power << " ";
+
+        }
+    }
+    if (N<0||N>10; M<1||M>10)
+    {    cout << "Invalid Entries" << endl;
+    }
     // TODO
 
     cout << endl;
